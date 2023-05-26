1328f7b5ad86b89808ec6d99dbfc547a5d029956
BurhanRaja <burhanraja02@yahoo.com>
2023-05-26 09:06:19 +0530

    added prisma configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "ct-dark-600": "#222",
+        "ct-dark-200": "#575757",
+        "ct-dark-100": "#6d6d6d",
+        "ct-blue-600": "#88abff",
+        "ct-blue-700": "#6a93f8",
+        "ct-yellow-600": "#f9d13e",
+      },
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          lg: "1125px",
+          xl: "1125px",
+          "2xl": "1125px",
+          "3xl": "1500px",
+        },
       },
     },
   },
   plugins: [],
 }
+
